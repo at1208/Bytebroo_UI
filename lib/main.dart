65e705cf998d8a7815ef99843936c82ea5d9ad23
aman tiwari <amantiwari12081999@gmail.com>
2019-06-24 05:21:09 +0530

    container added

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
-import './header.dart';
-main(){
+ main(){
   runApp(App());
 }
 
-class App extends StatelessWidget{
+class App extends  StatelessWidget{
  Widget build(BuildContext context){
-  return (
-    Header()
-    );
- 
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 255, 255, 255),
+          centerTitle: true,
+          title: Text('Bytebroo',
+           style: TextStyle(
+             color: Color.fromARGB(255, 30, 144, 255),
+             fontWeight: FontWeight.bold,
+             fontSize: 46.0,
+             
+                    ),
+                  ),
+               ),
+        body: Container(
+          height: 200,
+          width: 400,
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 255, 3, 100)),
+        ),
+             )
+           );
   }
 }
